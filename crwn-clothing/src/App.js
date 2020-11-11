82a2aef196f2f27901885a95ae8047272168a34e
taeboki <tae@Tae.localdomain>
2020-11-11 23:39:02 +0800

    updating app with shop page, with collection preview component and collection item component

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -2,20 +2,14 @@ import './App.css';
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
-
-const HatPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
-
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact={true} path='/' component={HomePage} />
-        <Route path='/hats' component={HatPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   )
