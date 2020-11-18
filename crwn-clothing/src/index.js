5ff7769168112f9492a66c27dab49174630c286b
taeboki <tae@Tae.localdomain>
2020-11-18 22:59:22 +0800

    setup redux

--- crwn-clothing/src/index.js
+++ crwn-clothing/src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import store from './redux/store';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
